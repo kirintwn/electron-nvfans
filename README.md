4c85141045504c837be532f0787d238cc5603a73
kirintw <cy91244@gmail.com>
2017-06-29 20:25:41 +0800

    add d3(v2) reference

---
+++ README.md
@@ -0,0 +1,6 @@
+### License
+The MIT License
+
+Copyright (c) 2017 Kirintw
+
+See the LICENSE file for details.
--- shellscripts/NvidiaCustom.sh
+++ shellscripts/NvidiaCustom.sh
@@ -9,31 +9,8 @@
 # https://github.com/cy91244/UbuntuFansControl #
 ################################################
 
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-
-# http://www.apache.org/licenses/LICENSE-2.0
-
-# Disclaimer of Warranty:
-# Unless required by applicable law or agreed to in writing, Licensor provides the Work
-# (and each Contributor provides its Contributions) on an "AS IS" BASIS, WITHOUT WARRANTIES
-# OR CONDITIONS OF ANY KIND, either express or implied, including, without limitation, any
-# warranties or conditions of TITLE, NON-INFRINGEMENT, MERCHANTABILITY, or FITNESS FOR A
-# PARTICULAR PURPOSE. You are solely responsible for determining the appropriateness of
-# using or redistributing the Work and assume any risks associated with Your exercise of
-# permissions under this License.
-
-# Limitation of Liability:
-# In no event and under no legal theory, whether in tort (including negligence), contract,
-# or otherwise, unless required by applicable law (such as deliberate and grossly negligent
-# acts) or agreed to in writing, shall any Contributor be liable to You for damages, including
-# any direct, indirect, special, incidental, or consequential damages of any character arising
-# as a result of this License or out of the use or inability to use the Work (including but
-# not limited to damages for loss of goodwill, work stoppage, computer failure or malfunction,
-# or any and all other commercial damages or losses), even if such Contributor has been advised
-# of the possibility of such damages.
-
+# Licensed under the MIT License
+# See the LICENSE file for details.
 
 #####################################################################
 #                          *** IMPORTANT ***                        #
